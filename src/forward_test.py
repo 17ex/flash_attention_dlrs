3c82a5954d6242bff2907f7ef11b9f83332087be
17ex <93944145+17ex@users.noreply.github.com>
2024-06-29 01:52:46 +0200

    Add test for forward and backward pass multi-head and batches

--- src/forward_test.py
+++ src/forward_test.py
@@ -2,24 +2,34 @@ from flash_attention import flash_attention_forward, flash_attention_backward
 from flash_attention_openai_tutorial import attention as openai_attention
 import torch
 
-B = 16
-H = 16
+# This file is a script that should test the Flash Attention implementation
+# by comparing the outputs and gradients to those of torch's attention implementation.
+# Running this takes ~30s on my system.
+# Reduce numbers below to reduce runtime.
+
+B = 32
+H = 32
 N = 256
 d = 128
-NUM_TESTS = 100
+NUM_TESTS = 200
 
-# NVIDIA GA102 GPUs
+# For NVIDIA GA102 GPUs
 # They have 128KB L1 (SRAM) per SM
 SRAM = 64 * 1024 # Something is wrong either here or later on. Hotfix
         # TODO Either this value is wrong, some calculation later on,
         # or I'm using more memory than I should.
 # Based on triton.runtime.errors.OutOfResources message,
 # it seems this is actually just 99*1024 ?
+# So maybe 64KB is not that wrong after all?
 
+# Use CUDA. I don't have a non-cuda (eg. ROCm) GPU on which to test this,
+# so I have no clue if my implementation supports non-CUDA targets.
 gpu = torch.device('cuda')
 
 test_result_fwd = torch.zeros(NUM_TESTS, dtype=torch.int32)
-test_result_bwd = torch.zeros(NUM_TESTS, dtype=torch.int32)
+test_result_bwd_Q = torch.zeros(NUM_TESTS, dtype=torch.int32)
+test_result_bwd_K = torch.zeros(NUM_TESTS, dtype=torch.int32)
+test_result_bwd_V = torch.zeros(NUM_TESTS, dtype=torch.int32)
 
 for test in range(NUM_TESTS):
     torch.manual_seed(test)
@@ -34,19 +44,13 @@ for test in range(NUM_TESTS):
             M=SRAM,
             dev=gpu
             )
-    # O_openai = openai_attention(Q[None, None, :, :].to(dtype=torch.float16), K[None, None, :, :].to(dtype=torch.float16), V[None, None, :, :].to(dtype=torch.float16), False, 1.0)
     if torch.allclose(O_torch, O_flash, atol=1e-4, rtol=1e-5):
         test_result_fwd[test] = 1
     else:
         print(O_torch)
         print(O_flash)
-        # print(Q@K.T)
-        # print(O_openai)
 
-    # TODO
-    # Get rid of this when backward pass supports batch/multihead.
-    # Just disabling bwd tests until then.
-    continue
+    dO = torch.randn_like(O_torch)
 
     dQ_torch, dK_torch, dV_torch = torch.autograd.grad(O_torch, (Q, K, V), dO)
 
@@ -61,17 +65,21 @@ for test in range(NUM_TESTS):
             dev=gpu
             )
 
-    test_result_bwd[test] = (
-            torch.allclose(dQ_torch, dQ_flash, atol=2e-4, rtol=1e-5) and
-            torch.allclose(dK_torch, dK_flash, atol=2e-4, rtol=1e-5) and
-            torch.allclose(dV_torch, dV_flash, atol=2e-4, rtol=1e-5)
-            )
+    # Somewhere around this ballpark are the tolerances.
+    # Some tests begin to fail for a little tighter (absolute) tolerances.
+    test_result_bwd_Q[test] = torch.allclose(dQ_torch, dQ_flash, atol=9e-4, rtol=1e-5)
+    test_result_bwd_K[test] = torch.allclose(dK_torch, dK_flash, atol=7e-4, rtol=1e-5)
+    test_result_bwd_V[test] = torch.allclose(dV_torch, dV_flash, atol=7e-5, rtol=1e-5)
 
     # TODO
-    # With 1e-4 atol, 87 tests pass only, with 2e-4 atol, all pass.
-    # However, rarely 86 tests (bwd, with 1e-4 atol) pass.
-    # This means that outputs of either the torch or my implementation
-    # are non-deterministic and there might be something going on there.
+    # Sometimes (rarely), some tests seem to fail nondeterministically.
+    # This means that either in my implementation or the torch one,
+    # the outputs can differ non-deterministically by a very small amount.
+    # I don't know why, how, or what this means, but this being non-deterministic
+    # seems bad.
+    # Find out if this happens in torch's attention implementation or in my flash attention impl.
 
 print(f"{test_result_fwd.sum().item()} out of {NUM_TESTS} forward tests succeeded!")
-print(f"{test_result_bwd.sum().item()} out of {NUM_TESTS} backward tests succeeded!")
+print(f"{test_result_bwd_Q.sum().item()} out of {NUM_TESTS} Q backward tests succeeded!")
+print(f"{test_result_bwd_K.sum().item()} out of {NUM_TESTS} K backward tests succeeded!")
+print(f"{test_result_bwd_V.sum().item()} out of {NUM_TESTS} V backward tests succeeded!")
