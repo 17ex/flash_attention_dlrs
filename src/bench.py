c8688ef8cc3c91cefcdea5e7365424d3b80c4857
17ex <93944145+17ex@users.noreply.github.com>
2024-07-02 12:08:02 +0200

    Run benchmark for plotting when benchmark data doesn't exist yet
    Also added an option to run benchmark again anyways.

--- src/bench.py
+++ src/bench.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import triton
 from flash_attention_torch import FlashAttention, FlashAttentionDeterministic
@@ -5,93 +6,111 @@ from flash_attn import flash_attn_func
 from flash_attention_openai_tutorial import _attention as openai_attention
 
 B = 8
-H = 8
-d = 32
+H = 16
+d = 128
+N_MIN_log = 7  # OpenAI Triton requires at least 2^7
+N_MAX_log = 15
 
+BENCH_DIR = "bench_out"
+
+# This can't be changed at the moment,
+# some implementations don't work with fp8, and some don't work with fp32.
 DTYPE = torch.float16
-MODES = ["fwd", "bwd"]
-# MODES = ["fwd"]
-# MODES = ["bwd"]
-torch.manual_seed(42)
 
-# CUDA only
-gpu = torch.device('cuda')
+MODES = ["bwd"]
+# MODES = ["fwd", "bwd"]
 
-bench_configs: list[triton.testing.Benchmark] = []
-for mode in MODES:
-    dtype_str = str(DTYPE).split('.')[1]
-    bench_configs.append(
-        triton.testing.Benchmark(
-            x_names=["N"],
-            x_vals=[2**i for i in range(7, 16)], # OpenAI Triton requires at least 2^7
-            line_arg="provider",
-            line_vals=[f"my-triton-det-{dtype_str}", f"my-triton-indet-{dtype_str}", f"daolab-{dtype_str}", f"openai-{dtype_str}",
-                       f"torch-fa-{dtype_str}", f"torch-xformers-{dtype_str}", f"torch-math-{dtype_str}"],
-            line_names=[f"My Triton ~FA-2 Det. [{dtype_str.upper()}]", f"My Triton ~FA-2 Indet. [{dtype_str.upper()}]",
-                        f"DaoLab FA-2 [{dtype_str.upper()}]", f"OpenAI Triton FA-2 [{dtype_str.upper()}]",
-                        f"Torch FA-2 [{dtype_str.upper()}]", f"Torch xFormers [{dtype_str.upper()}]", f"Torch Math [{dtype_str.upper()}]"],
-            styles=[("red", "-"), ("red", "--"), ("blue", "-"), ("green", "-"),
-                    ("black", "-"), ("black", "--"), ("black", "-.")],
-            xlabel="N: Context size/Sequence length",
-            ylabel="Mean Runtime [ms]",
-            plot_name=f"fused-attention-B{B}-B{H}-d{d}-{mode}-{dtype_str}",
-            args={
-                "B": B,
-                "H": H,
-                "d": d,
-                "dtype": DTYPE,
-                "mode": mode
-            },
-        ))
+def run_bench(B, H, d, modes=MODES, N_min_log=N_MIN_log, N_max_log=N_MAX_log, dtype=torch.float16) -> None:
+    if dtype != torch.float16:
+        raise ValueError("Benchmark only supports torch.float16 dtype")
+    torch.manual_seed(42)
+    # CUDA only
+    gpu = torch.device('cuda')
+    os.makedirs(BENCH_DIR, exist_ok=True)
+    bench_configs: list[triton.testing.Benchmark] = []
+    for mode in modes:
+        dtype_str = str(DTYPE).split('.')[1]
+        bench_configs.append(
+            triton.testing.Benchmark(
+                x_names=["N"],
+                x_vals=[2**i for i in range(N_min_log, N_max_log + 1)],
+                line_arg="provider",
+                line_vals=[f"my-triton-det-{dtype_str}", f"my-triton-indet-{dtype_str}", f"daolab-{dtype_str}", f"openai-{dtype_str}",
+                           f"torch-fa-{dtype_str}", f"torch-xformers-{dtype_str}", f"torch-math-{dtype_str}"],
+                line_names=[f"My Triton ~FA-2 Det. [{dtype_str.upper()}]", f"My Triton ~FA-2 Indet. [{dtype_str.upper()}]",
+                            f"DaoLab FA-2 [{dtype_str.upper()}]", f"OpenAI Triton FA-2 [{dtype_str.upper()}]",
+                            f"Torch FA-2 [{dtype_str.upper()}]", f"Torch xFormers [{dtype_str.upper()}]", f"Torch Math [{dtype_str.upper()}]"],
+                styles=[("red", "-"), ("red", "--"), ("blue", "-"), ("green", "-"),
+                        ("black", "-"), ("black", "--"), ("black", "-.")],
+                xlabel="N: Context size/Sequence length",
+                ylabel="Mean Runtime [ms]",
+                plot_name=f"fused-attention-B{B}-H{H}-d{d}-{mode}-{dtype_str}",
+                args={
+                    "B": B,
+                    "H": H,
+                    "d": d,
+                    "dtype": DTYPE,
+                    "mode": mode
+                },
+            ))
 
 
-# TODO ensure that all kernels are run once before benching
-@triton.testing.perf_report(bench_configs)
-def bench_flash_attention(B, H, N, d, mode, dtype, provider):
-    assert mode in MODES
-    warmup = 25
-    rep = 100
-    try:
-        Q = torch.randn(B, H, N, d, dtype=DTYPE, device=gpu, requires_grad=True)
-        K = torch.randn(B, H, N, d, dtype=DTYPE, device=gpu, requires_grad=True)
-        V = torch.randn(B, H, N, d, dtype=DTYPE, device=gpu, requires_grad=True)
-        if "my-triton" in provider:
-            if "my-triton-indet" in provider:
-                def fwd_fn() -> torch.Tensor: return FlashAttention.apply(Q, K, V)
-            else:
-                def fwd_fn() -> torch.Tensor: return FlashAttentionDeterministic.apply(Q, K, V)
-        elif "daolab" in provider:
-            def fwd_fn() -> torch.Tensor: return flash_attn_func(Q, K, V)
-        elif "torch" in provider:
-            if "torch-fa" in provider:
-                sdpbackend = torch.nn.attention.SDPBackend.FLASH_ATTENTION
-            elif "torch-xformers" in provider:
-                sdpbackend = torch.nn.attention.SDPBackend.EFFICIENT_ATTENTION
-            elif "torch-math" in provider:
-                sdpbackend = torch.nn.attention.SDPBackend.MATH
+    @triton.testing.perf_report(bench_configs)
+    def bench_flash_attention(B, H, N, d, mode, dtype, provider):
+        assert mode in modes
+        warmup = 25
+        rep = 100
+        try:
+            Q = torch.randn(B, H, N, d, dtype=DTYPE, device=gpu, requires_grad=True)
+            K = torch.randn(B, H, N, d, dtype=DTYPE, device=gpu, requires_grad=True)
+            V = torch.randn(B, H, N, d, dtype=DTYPE, device=gpu, requires_grad=True)
+            if "my-triton" in provider:
+                if "my-triton-indet" in provider:
+                    def fwd_fn() -> torch.Tensor: return FlashAttention.apply(Q, K, V)
+                else:
+                    def fwd_fn() -> torch.Tensor: return FlashAttentionDeterministic.apply(Q, K, V)
+            elif "daolab" in provider:
+                def fwd_fn() -> torch.Tensor: return flash_attn_func(Q, K, V)
+            elif "torch" in provider:
+                if "torch-fa" in provider:
+                    sdpbackend = torch.nn.attention.SDPBackend.FLASH_ATTENTION
+                elif "torch-xformers" in provider:
+                    sdpbackend = torch.nn.attention.SDPBackend.EFFICIENT_ATTENTION
+                elif "torch-math" in provider:
+                    sdpbackend = torch.nn.attention.SDPBackend.MATH
+                else:
+                    raise ValueError()
+                def fwd_fn() -> torch.Tensor:
+                    with torch.nn.attention.sdpa_kernel(sdpbackend):
+                        return torch.nn.functional.scaled_dot_product_attention(Q, K, V, scale=1)
+            elif "openai" in provider:
+                def fwd_fn() -> torch.Tensor: return openai_attention.apply(Q, K, V, False, 1.3)
             else:
                 raise ValueError()
-            def fwd_fn() -> torch.Tensor:
-                with torch.nn.attention.sdpa_kernel(sdpbackend):
-                    return torch.nn.functional.scaled_dot_product_attention(Q, K, V, scale=1)
-        elif "openai" in provider:
-            def fwd_fn() -> torch.Tensor: return openai_attention.apply(Q, K, V, False, 1.3)
-        else:
-            raise ValueError()
 
-        if mode == "bwd":
-            O: torch.Tensor = fwd_fn()
-            dO = torch.randn_like(O)
-            def bench_fn() -> torch.Tensor: return O.backward(dO, retain_graph=True)
-        else:
-            bench_fn = fwd_fn
+            if mode == "bwd":
+                O: torch.Tensor = fwd_fn()
+                dO = torch.randn_like(O)
+                def bench_fn() -> torch.Tensor: return O.backward(dO, retain_graph=True)
+            else:
+                bench_fn = fwd_fn
 
-        print(f"Benchmarking {mode} (N={N}, H={H}, B={B}, d={d}) for {provider} ...")
-        ms = triton.testing.do_bench(bench_fn, warmup=warmup, rep=rep)
-    except torch.cuda.OutOfMemoryError:
-        ms = float('NaN')
-    return ms
+            print(f"Benchmarking {mode} (N={N}, H={H}, B={B}, d={d}) for {provider} ...")
+            ms = triton.testing.do_bench(bench_fn, warmup=warmup, rep=rep)
+        except torch.cuda.OutOfMemoryError:
+            ms = float('NaN')
+        # except triton.runtime.errorsOutOfMemoryError:
+            # ms = float('NN')
+        except triton.runtime.errors.OutOfResources:
+            ms = float('NaN')
+        except RuntimeError as e:
+            print(f"{provider} raised the following error:")
+            print(e)
+            ms = float('NaN')
+        return ms
 
+
+    bench_flash_attention.run(save_path=BENCH_DIR + "/", print_data=True)
 
 if __name__ == "__main__":
-    bench_flash_attention.run(save_path="bench_out/", print_data=True)
+    run_bench(B, H, d)
--- src/plot_bench_results.py
+++ src/plot_bench_results.py
@@ -3,6 +3,7 @@ import re
 import torch
 import numpy as np
 import pandas as pd
+from bench import run_bench
 import matplotlib.pyplot as plt
 from tueplots.constants import markers
 from tueplots.constants.color import palettes
@@ -24,6 +25,9 @@ y_log = True
 N_MIN = 2**9
 N_MAX = 2**16
 
+run_bench_if_not_exists = True
+bench_overwrite_if_exists = False
+
 include_det = True
 include_indet = True
 
@@ -36,21 +40,37 @@ plt.rcParams.update(axes.lines())
 
 def get_bench_filename(B, H, d, mode, dtype) -> str:
     dtype_str = str(dtype).split('.')[1]
-    return f"{BENCH_DIR}/fused-attention-B{B}-B{H}-d{d}-{mode}-{dtype_str}.csv"
+    return f"{BENCH_DIR}/fused-attention-B{B}-H{H}-d{d}-{mode}-{dtype_str}.csv"
 
 
 def get_plot_filename(B, H, d, mode, dtype) -> str:
     dtype_str = str(dtype).split('.')[1]
     det_str = "-withdet" if include_det else ""
     log_str = "-ylog" if y_log else ""
-    return f"{PLOT_DIR}/plot-fused-attention-B{B}-B{H}-d{d}-{mode}-{dtype_str}{det_str}{log_str}.pdf"
+    return f"{PLOT_DIR}/plot-fused-attention-B{B}-H{H}-d{d}-{mode}-{dtype_str}{det_str}{log_str}.pdf"
 
 
-def get_bench_data(B, H, d, mode, dtype) -> pd.DataFrame:
-    df = pd.read_csv(
+def read_bench_csv(B, H, d, mode, dtype) -> pd.DataFrame:
+    return pd.read_csv(
             get_bench_filename(B, H, d, mode, dtype),
             sep=",",
             )
+
+
+def get_bench_data(B, H, d, mode, dtype) -> pd.DataFrame:
+    ran_bench = False
+    try:
+        if bench_overwrite_if_exists:
+            # Run bench regardless whether it exists or not
+            ran_bench = True
+            run_bench(B, H, d, [mode, ], dtype=dtype)
+        df = read_bench_csv(B, H, d, mode, dtype)
+    except FileNotFoundError as e:
+        if not ran_bench and run_bench_if_not_exists:
+            run_bench(B, H, d, [mode, ], dtype=dtype)
+            df = read_bench_csv(B, H, d, mode, dtype)
+        else:
+            raise e
     df['N'] = df['N'].astype(np.int32)
     df = df[df['N'] >= N_MIN]
     df = df[df['N'] <= N_MAX]
@@ -66,6 +86,8 @@ def plot_title(B, H, d, mode, dtype) -> str:
             dtype_str = "FP16"
         case torch.float32:
             dtype_str = "FP32"
+        case _:
+            raise ValueError(f"This dtype is not supported: {dtype}")
     return f"Attention {mode_str} (B={B}, H={H}, d={d}), {dtype_str}"
 
 
@@ -103,4 +125,6 @@ def plot_bench_data(B, H, d, mode, dtype):
         plt.legend()
         plt.savefig(get_plot_filename(B, H, d, mode, dtype))
 
-plot_bench_data(8, 8, 32, "fwd", torch.float16)
+
+if __name__ == "__main__":
+    plot_bench_data(B, H, d, mode, dtype)
